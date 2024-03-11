a38eb48c2646c3c08659472426bbfabcb600ef38
Carlos André <carlosandre1572@gmail.com>
2024-03-11 19:26:33 -0300

    Refactor recipesService.ts to include getRecipesByIngredients function

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import * as ingredients from "./ingredientsService";
+import * as recipes from "./recipesService";
+import * as preparations from "./preparationsService";
 
 export const services = {
   ingredients,
+  recipes,
+  preparations,
   storage: {
     imagePath:
       "https://arffovfmbkkmievfzhek.supabase.co/storage/v1/object/public/ingredients",
--- src/services/recipesService.ts
+++ src/services/recipesService.ts
@@ -1,11 +1,37 @@
-import { supabase } from "./supabase"
+import { supabase } from "./supabase";
 
 async function findByIngredientsIds(ids: string[]) {
   const { data } = await supabase
     .rpc("recipes_by_ingredients", { ids })
-    .returns<RecipeResponse[]>()
+    .returns<RecipeResponse[]>();
 
-  return data ?? []
+  return data ?? [];
+}
+
+async function getRecipesByIngredients(ids: string[]) {
+  const { data: filteredIngredients } = await supabase
+    .from("recipes_ingredients")
+    .select("recipe_id")
+    .eq("ingredient_id", ids);
+  // console.log("filteredIngredients", filteredIngredients);
+
+  // If there are filtered ingredients, join and select recipes
+  if (filteredIngredients?.length ?? 0 > 0) {
+    const recipeIds = filteredIngredients?.map(
+      (ingredient) => ingredient.recipe_id
+    );
+    // console.log("Selecting recipes query:", recipeIds);
+    const { data: recipes } = await supabase
+      .from("recipes")
+      .select("*")
+      .in("id", recipeIds ?? [])
+      .returns<RecipeResponse[]>();
+    // console.log("recipes", recipes);
+    return recipes ?? [];
+  } else {
+    // Handle case where no ingredients are found
+    return [];
+  }
 }
 
 async function show(id: string) {
@@ -14,9 +40,9 @@ async function show(id: string) {
     .select()
     .eq("id", id)
     .returns<RecipeResponse>()
-    .single()
+    .single();
 
-  return data
+  return data;
 }
 
-export { findByIngredientsIds, show }
+export { findByIngredientsIds, show, getRecipesByIngredients };
