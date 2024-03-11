714181fda84197203c93838f68d5bd766ad2f531
Carlos André <carlosandre1572@gmail.com>
2024-03-11 09:18:25 -0300

    feat: Add services and storage configuration

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import * as ingredients from "./ingredientsService";
+
+export const services = {
+  ingredients,
+  storage: {
+    imagePath:
+      "https://arffovfmbkkmievfzhek.supabase.co/storage/v1/object/public/ingredients",
+  },
+};
